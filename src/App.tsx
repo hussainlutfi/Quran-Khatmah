15ce9ddfdbc3ca0d7189de79916467e02cbfa733
7ussainlz <hussainlutfi@hotmail.com>
2024-05-28 00:36:34 +0300

    trys

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [number, setNumber] = useState<number>(0);
+
+  useEffect(() => {
+    const date: Date = new Date("2024-05-28");
+    const dateNumber: number = 308;
+
+    setNumber((dateNumber + CalculateDate(date)) % 360);
+  }, []);
+
+  function CalculateDate(startDate: Date): number {
+    const todayDate: Date = new Date();
+    return Math.floor(
+      //wrong
+      todayDate.getTime() - startDate.getTime() / (1000 * 3600 * 24)
+    );
+  }
 
   return (
-    <>
+    <div className="flex justify-between ">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 className="text-3xl font-bold">Hussain Lutfi</h1>
+        <h1 className="text-2xl">{number}</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <h1 className="text-3xl font-bold">Ali Wajdi</h1>
+        <h1 className="text-2xl">{number + 1}</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
